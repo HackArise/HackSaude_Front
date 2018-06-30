8cbecf97ffdd9da763265fd7f54fb2b275ddf45a
Lucas Malta <lucas50xd@hotmail.com>
2018-06-30 10:06:13 -0300

    adds static data

---
+++ info.js
@@ -0,0 +1,72 @@
+let pointsInfo = 
+[
+    {
+        labels: ["Psiquiatra", "Médico geral"],
+        data: [2, 1],
+        label: 'Quantidade de médicos',
+        lat: -15.8309,
+        long: -47.9952,
+        id: 1
+    },
+    {
+        labels: ["Dentista", "Fisioterapeuta"],
+        data: [1, 1],
+        label: 'Quantidade de médicos',
+        lat: -15.8315,
+        long: -48.005,
+        id: 2
+    },
+    {
+        labels: ["Cardiologista"],
+        data: [1],
+        label: 'Quantidade de médicos',
+        lat: -15.88,
+        long: -47.9940,
+        id: 3
+    },
+]
+
+let medicInfo = [
+    {
+        name: "Lucas Mota",
+        profession: "Psiquiatra",
+        journey : ['12:00', '18:00'],
+        observation: "Excelente médico, focado na área de tratamento de idosos",
+        pointId: 1
+    },
+    {
+        name: "Rafael Santos",
+        profession: "Psiquiatra",
+        journey : ['15:00', '19:00'],
+        observation: "Médico focado em tratar pessoas com transtornos depressivos",
+        pointId: 1
+    },
+    {
+        name: "Ricardo Fragelli",
+        profession: "Médico geral",
+        journey : ['12:00', '18:00'],
+        observation: "Excelente ficha de tratamento",
+        pointId: 1
+    },
+    {
+        name: "Bárbara rocha",
+        profession: "Dentista",
+        journey : ['08:00', '15:00'],
+        observation: "Especializada em clareamento",
+        pointId: 2
+    },
+    {
+        name: "Bruno de Lyra",
+        profession: "Fisioterapeuta",
+        journey : ['11:00', '17:00'],
+        observation: "Médico focado na reabilitação de pessoas que sofreram de um AVC",
+        pointId: 2
+    },
+    {
+        name: "Roberto dantas",
+        profession: "Cardiologista",
+        journey : ['16:00', '20:00'],
+        observation: "Bom histórico e atendimento",
+        pointId: 3
+    }
+]
